32edd9d8d0c8c82d7f98eacf50a9e1ee988ebafc
Guilherme Lino <guilhermelinosp@gmail.com>
2022-12-19 20:08:11 -0300

    feat: init new project

---
+++ README.md
@@ -0,0 +1,69 @@
+# <div align="center"> API Pizza Hub </div>
+
+</br>
+
+<div align="center">
+<p>🚧 It is in Development 🚧</p>
+
+</br>
+
+![image 1](/.github/image.png)
+
+</div>
+
+</br>
+
+## Technologies used in the project
+
+- Typescript
+- NodeJS
+- Express
+- Prisma
+- Swagger
+
+## Technologies used outside the project
+
+- Docker
+- insominia
+- Beekeeper
+
+## Technologies used to deploy
+
+- Heroku | API
+- Railway | Database
+
+## How to run the project
+
+- Clone this repository
+
+```shell
+git clone https://github.com/guilhermelinosx/api-pizza-hub.git
+```
+
+</br>
+
+- Start the Application in Development
+
+```shell
+yarn up
+```
+
+</br>
+
+- Stop the Application in Development
+
+```shell
+yarn down
+```
+
+</br>
+
+- Open bash in the Application
+
+```shell
+yarn bash
+```
+
+</br>
+
+- To closed bash in the Application, write "exit" in the terminal
---
+++ src/shared/router/index.ts
@@ -0,0 +1,23 @@
+import { Router } from 'express'
+import { CategoryRouter } from '../../modules/Categories/routes/CategoryRouter'
+import { OrderRouter } from '../../modules/Orders/routes/OrderRouter'
+import { ProductRouter } from '../../modules/Products/routes/ProductRouter'
+import { UserRouter } from '../../modules/Users/routes/UserRouter'
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocument from '../utils/swagger/swagger.json'
+import { ItemRouter } from '../../modules/Items/routes/ItemRouter'
+
+export const router = Router()
+
+router.use('/api/v1', UserRouter)
+router.use('/api/v1/categories', CategoryRouter)
+router.use('/api/v1/products', ProductRouter)
+router.use('/api/v1/orders', OrderRouter)
+router.use('/api/v1/items', ItemRouter)
+
+router.get('/', (_req, res) => {
+  res.send('API is running')
+})
+
+router.use('/api/v1/docs', swaggerUi.serve)
+router.get('/api/v1/docs', swaggerUi.setup(swaggerDocument))
